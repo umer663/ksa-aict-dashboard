edb778f4fc96a05e3476223dc994449933aa9d59
Umar Ikram <umar663@hotmail.com>
2025-09-19 03:33:55 +0500

    Setup GitHub Actions for Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   plugins: [react()],
+//   base: mode === 'production' ? '/ksa-aict-dashboard/' : '/',
+//   build: {
+//     outDir: 'dist',
+//     emptyOutDir: true,
+//   }
+// }))
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/ksa-aict-dashboard/' : '/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
-}))
+  base: '/ksa-aict-dashboard/',   // 👈 your repo name
+})
+
